2aec1aa188cf16d5040d5593ea49a7ce1c1db39c
Golgotha Aksanti <goloking777@gmail.com>
2022-08-01 05:06:27 +0300

    first commit

---
+++ src/components/Row.tsx
@@ -0,0 +1,48 @@
+type Todo = {
+  id: string;
+  task: string;
+  isCompleted: boolean;
+};
+
+type TodoProps = {
+  todo: Todo;
+  handleDeleteTodo: (id: string) => void;
+  handleCheckTodo: (id: string) => void;
+};
+
+export const Row = ({
+  todo: { task, isCompleted, id },
+  handleDeleteTodo,
+  handleCheckTodo,
+}: TodoProps) => {
+  return (
+    <div
+      className={`flex transition duration-200 mb-2 w-full rounded  p-4 justify-between items-center ${
+        isCompleted ? 'bg-gray-200 ' : 'bg-green-300/50'
+      }`}
+    >
+      <p
+        className={`ml-2  text-xl font-sans font-normal first-letter:capitalize
+         ${isCompleted ? 'text-gray-600/50 line-through' : 'text-gray-900/50'}
+        `}
+      >
+        {task}
+      </p>
+      <div className='w-1/6 flex justify-between items-center mr-2'>
+        <button
+          className='h-7 w-7 bg-red-400/50 flex item-center text-white font-semibold transition duration-75 rounded hover:bg-red-500/50 justify-center'
+          aria-label='Delete a todo'
+          onClick={() => handleDeleteTodo(id)}
+        >
+          x
+        </button>
+        <input
+          className='h-7 w-7'
+          type='checkbox'
+          checked={isCompleted}
+          onChange={() => handleCheckTodo(id)}
+        />
+      </div>
+    </div>
+  );
+};
